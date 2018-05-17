efaf3c2d5b632c39919507bdce24b9548ef67bdb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-17 21:09:29 +0000

    automatically backed up by learn

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
   has_many :ordered_products
-  has_many :products, :through :ordered_products
+  has_many :products, :through => :ordered_products
 end
