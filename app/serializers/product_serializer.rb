6f0bb25b04128fc5f42d94dd68b058c5d8a79c1b
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-17 20:53:55 +0000

    automatically backed up by learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :description, :inventory
-  has_many :orders, through: => :ordered_products
+  has_many :orders, :through => :ordered_products
 end
